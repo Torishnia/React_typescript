12d90779b5c8a175ae9d0f6c5cc9dfa50d9ab8b9
Torishnia <valeri.torishnya@gmail.com>
2023-01-18 17:51:39 +0300

    Add data loading from the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {Product} from './components/Product';
-import {products} from './data/products';
+import { IProduct } from './models';
+import axios from 'axios';
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  async function fetchProducts() {
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5');
+    setProducts(response.data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
 
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      <Product product={ products[0] }/>
-      <Product product={ products[1] }/>
+      { products.map(product => <Product product={product} key={product.id} />) }
     </div>
   )
 }
