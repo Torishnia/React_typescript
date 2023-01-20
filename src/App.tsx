6af6f530de84604a7cff9d86463aa99e4e8f8f96
Torishnia <valeri.torishnya@gmail.com>
2023-01-20 23:28:50 +0300

    Add Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Navigation } from './components/Navigation';
 import { AboutPage } from './pages/AboutPage';
 import { ProductsPage } from './pages/ProductsPage';
 
 function App() {
   return (
-    <Routes>
+    <>
+      <Navigation/>
+      
+      <Routes>
       <Route path = '/' element = { <ProductsPage/> }/>
       <Route path = '/about' element = { <AboutPage/> }/>
-    </Routes>
+      </Routes>
+    </>
   )
 }
 
