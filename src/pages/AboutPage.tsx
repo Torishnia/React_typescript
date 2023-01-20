ba879345df74898951570f92847525a681e1d2ec
Torishnia <valeri.torishnya@gmail.com>
2023-01-20 23:06:46 +0300

    Add routing

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function AboutPage() {
+  return (
+    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus officia omnis maiores deserunt eius? Voluptate rem distinctio accusantium optio id, animi repellat unde aliquid iusto! Nam ratione placeat ipsam reprehenderit aperiam, culpa excepturi eius libero quidem perferendis officiis ab sit omnis earum maiores suscipit tenetur eligendi veniam commodi! Modi, numquam?</div>
+  )
+}
