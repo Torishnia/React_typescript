38b7ab8b74b17c01bc2ed889805ce4c8ff25871b
Torishnia <valeri.torishnya@gmail.com>
2023-01-20 20:51:12 +0300

    Add another option to close modal window

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
       { error && <ErrorMessage error = { error } /> }
       { products.map(product => <Product product = { product } key = { product.id } />) }
 
-      {modal && <Modal title = 'Create new product'>
+      {modal && <Modal title = 'Create new product' onClose = {() => setModal(false)}>
         <CreateProduct onCreate = { createHandler }/>
       </Modal>}
     </div>
