02ae88208a587329e01a96073b0c9d4a75d088d0
Torishnia <valeri.torishnya@gmail.com>
2023-01-20 20:17:16 +0300

    Create new product

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
+import { IProduct } from '../models';
+import axios from 'axios';
+import { ErrorMessage } from './ErrorMessage';
+
+const productData: IProduct = {
+    title: '',
+    price: 13.5,
+    description: 'lorem ipsum set',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic',
+    rating: {
+      rate: 42,
+      count: 10
+    }
+}
 
 export function CreateProduct() {
   const [value, setValue] = useState('');
+  const [error, setError] = useState('');
 
-  const submitHandler = (event: React.FormEvent) => {
+  const submitHandler = async (event: React.FormEvent) => {
     event.preventDefault();
+    setError('');
+
+    if (value.trim().length === 0) {
+      setError('Please enter valid title');
+      return
+    }
+
+    productData.title = value;
+    const response = await axios.post<IProduct>('https://fakestoreapi.com/products', productData);
   }
 
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -21,6 +46,8 @@ export function CreateProduct() {
         onChange = { changeHandler }
       />
 
+      {error && <ErrorMessage error = {error}/>}
+      
       <button type = 'submit' className = 'py-2 px-4 border bg-green-400 hover:text-white'>Create</button>
     </form>
   )
